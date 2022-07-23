d65c99a7cafb3cc8187db2285d4ca5fd9bd334e1
Mouad El Amrani <mouaadamrani@gmail.com>
2022-07-23 21:34:44 +0100

    animation 2 test

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-/* Check External Source: bvambient.js */
-document.addEventListener("DOMContentLoaded", function () {
-    var demo1 = new BVAmbient({
-        selector: "#ambient",
-        fps: 60,
-        max_transition_speed: 12000,
-        min_transition_speed: 8000,
-        particle_number: 30,
-        particle_maxwidth: 60,
-        particle_minwidth: 10,
-        particle_radius: 50,
-        particle_opacity: true,
-        particle_colision_change: true,
-        particle_background: "#58c70c",
-        particle_image: {
-            image: false,
-            src: ""
-        },
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    particle_number: "15"
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    particle_number: "10"
-                }
-            }
-        ]
-    });
+import * as datGui from "https://cdn.skypack.dev/dat.gui@0.7.7";
+
+const state = {
+  fps: 60,
+  color: "#0f0",
+  charset: "0123456789ABCDEF"
+};
+
+const gui = new datGui.GUI();
+const fpsCtrl = gui.add(state, "fps").min(1).max(120).step(1);
+gui.addColor(state, "color");
+gui.add(state, "charset");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let w, h, p;
+const resize = () => {
+  w = canvas.width = innerWidth;
+  h = canvas.height = innerHeight;
+
+  p = Array(Math.ceil(w / 10)).fill(0);
+};
+window.addEventListener("resize", resize);
+resize();
+
+const random = (items) => items[Math.floor(Math.random() * items.length)];
+
+const draw = () => {
+  ctx.fillStyle = "rgba(0,0,0,.05)";
+  ctx.fillRect(0, 0, w, h);
+  ctx.fillStyle = state.color;
+
+  for (let i = 0; i < p.length; i++) {
+    let v = p[i];
+    ctx.fillText(random(state.charset), i * 10, v);
+    p[i] = v >= h || v >= 10000 * Math.random() ? 0 : v + 10;
+  }
+};
+
+let interval = setInterval(draw, 1000 / state.fps);
+fpsCtrl.onFinishChange((fps) => {
+  console.log(fps);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(draw, 1000 / fps);
 });
