4fdc0a110a2b753a0eb57baebd8a098650838953
Mouad El Amrani <mouaadamrani@gmail.com>
2022-07-23 21:50:32 +0100

    animation 2 test 3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import * as datGui from "https://cdn.skypack.dev/dat.gui@0.7.7";
 
 const state = {
-  fps: 60,
-  color: "#0f0",
-  charset: "0123456789ABCDEF"
+    fps: 60,
+    color: "#0f0",
+    charset: "0123456789ABCDEF"
 };
 
 const gui = new datGui.GUI();
@@ -16,10 +16,10 @@ const ctx = canvas.getContext("2d");
 
 let w, h, p;
 const resize = () => {
-  w = canvas.width = innerWidth;
-  h = canvas.height = innerHeight;
+    w = canvas.width = innerWidth;
+    h = canvas.height = innerHeight;
 
-  p = Array(Math.ceil(w / 10)).fill(0);
+    p = Array(Math.ceil(w / 10)).fill(0);
 };
 window.addEventListener("resize", resize);
 resize();
@@ -27,22 +27,22 @@ resize();
 const random = (items) => items[Math.floor(Math.random() * items.length)];
 
 const draw = () => {
-  ctx.fillStyle = "rgba(0,0,0,.05)";
-  ctx.fillRect(0, 0, w, h);
-  ctx.fillStyle = state.color;
+    ctx.fillStyle = "rgba(0,0,0,.05)";
+    ctx.fillRect(0, 0, w, h);
+    ctx.fillStyle = state.color;
 
-  for (let i = 0; i < p.length; i++) {
-    let v = p[i];
-    ctx.fillText(random(state.charset), i * 10, v);
-    p[i] = v >= h || v >= 10000 * Math.random() ? 0 : v + 10;
-  }
+    for (let i = 0; i < p.length; i++) {
+        let v = p[i];
+        ctx.fillText(random(state.charset), i * 10, v);
+        p[i] = v >= h || v >= 10000 * Math.random() ? 0 : v + 10;
+    }
 };
 
 let interval = setInterval(draw, 1000 / state.fps);
 fpsCtrl.onFinishChange((fps) => {
-  console.log(fps);
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(draw, 1000 / fps);
+    console.log(fps);
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(draw, 1000 / fps);
 });
